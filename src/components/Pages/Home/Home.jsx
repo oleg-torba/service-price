d4330d81688224bc051888da21221c4c8b50c1dc
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-07-09 22:11:03 +0300

    Update Home.jsx

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,40 +1,43 @@
-
-
-import phone from "./phone.jpg"
+import phone from './phone.jpg';
 import heroCss from './Hero.module.css';
 
-export function Home () {
-    return (
-        <><div>
-            <ul className={heroCss.category}>
-                <li>
-                    <p className={heroCss.title}>Apple</p>
-                </li>
-                <li>
-                    <p className={heroCss.title}>Huawei</p>
-                </li>
-                <li>
-                    <p className={heroCss.title}>Oppo</p>
-                </li>
-                <li>
-                    <p className={heroCss.title}>Realme</p>
-                </li>
-                <li>
-                    <p className={heroCss.title}>Samsung</p>
-                </li>
-                <li>
-                    <p className={heroCss.title}>Xiaomi</p>
-                </li>
-            </ul>
-        </div><div className={heroCss.serviceBlock}>
-                <div>
-                    <h1 className={heroCss.repairTitle}>Ремонт та сервіс</h1>
-                    <p className={heroCss.repairTitleSecond}>Поремонтуємо твій гаджет швидко, якісно та надійно</p>
-                    <button className={heroCss.button}>Консультація</button>
-                </div>
-                <div className={heroCss.imgBlock}>
-                    <img className={heroCss.heroImg} src={phone} alt="phone" />
-                </div>
-            </div></>
-    )
+export function Home() {
+  return (
+    <>
+      <div>
+        <ul className={heroCss.category}>
+          <li>
+            <p className={heroCss.title}>Apple</p>
+          </li>
+          <li>
+            <p className={heroCss.title}>Huawei</p>
+          </li>
+          <li>
+            <p className={heroCss.title}>Oppo</p>
+          </li>
+          <li>
+            <p className={heroCss.title}>Realme</p>
+          </li>
+          <li>
+            <p className={heroCss.title}>Samsung</p>
+          </li>
+          <li>
+            <p className={heroCss.title}>Xiaomi</p>
+          </li>
+        </ul>
+      </div>
+      <div className={heroCss.serviceBlock}>
+        <div>
+          <h1 className={heroCss.repairTitle}>Ремонт та сервіс</h1>
+          <p className={heroCss.repairTitleSecond}>
+            Поремонтуємо твій гаджет швидко, якісно та надійно
+          </p>
+          <button className={heroCss.button}>Консультація</button>
+        </div>
+        <div className={heroCss.imgBlock}>
+          <img className={heroCss.heroImg} src={phone} alt="phone" />
+        </div>
+      </div>
+    </>
+  );
 }
